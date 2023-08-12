ae2b67b4a973298ae5265a31b2d4bf0a59be7ab1
emmittjames <emmittjames1@gmail.com>
2023-08-12 00:53:56 -0400

    Added staticfiles storage

--- transferguide/settings.py
+++ transferguide/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
