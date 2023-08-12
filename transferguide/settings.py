1a0fcb59f4b9e4ea6af6b46fec9f82bb0ca9f18c
emmittjames <emmittjames1@gmail.com>
2023-08-11 21:40:10 -0400

    Changed some settings

--- transferguide/settings.py
+++ transferguide/settings.py
@@ -18,17 +18,18 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#x6@#b=amu1t66jbcs9kb5zym!-**m_3h7bi&av_x#yr=1ca8'
+# SECRET_KEY = 'django-insecure-y#x6@#b=amu1t66jbcs9kb5zym!-**m_3h7bi&av_x#yr=1ca8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'transfer-guide.herokuapp.com', 'testserver']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'transferguide.herokuapp.com', 'testserver', 'uvatransferguide-ba0013ae3635.herokuapp.com']
 
 # storing session data in the database for shopping cart
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
@@ -207,7 +208,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-SITE_ID = 1
+SITE_ID = 4
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
